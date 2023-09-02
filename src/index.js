3198294dc97c7a93fa071a3c2891745bc485f876
tryoxiss <tryoxiss@gmail.com>
2023-09-02 16:48:05 -0700

    ✨ UwU

--- src/index.js
+++ src/index.js
@@ -3,15 +3,57 @@ import m from "mithril";
 let root = document.body
 
 m.render(root, [
-    m("h1", "Hello constellation")
+    m("h1", "Hello constellation!")
 ]);
 
 var Splash =
 {
     view: function()
     {
-        return m("")
+        return m("", [
+            m("a", { href: "#/edit" }, "Go inside!")
+        ])
     }
 }
 
-m.route()
+var FileExplorer =
+{
+    view: function()
+    {
+        return m("ul", { class: "file-explorer" }, [
+            m("li", "file :3")
+        ])
+    }
+}
+
+var MainView =
+{
+    view: function()
+    {
+        return m("", [
+            m("h1", "Hello"),
+            m(FileExplorer),
+            m("div", { class: "content-area markdown-area yes-do-render", contenteditable: true })
+        ])
+    }
+}
+
+var GraphView =
+{
+    view: function()
+    {
+        return m("div", [
+            m("h1", "Not implemented yet"),
+            m(FileExplorer),
+            m("p", "Sorry :/"),
+            m("a", { href: "#/edit" }, "go back")
+        ])
+    }
+}
+
+m.route(root, "/splash",
+{
+    "/splash": Splash,
+    "/edit": MainView,
+    "/graph": GraphView,
+})
